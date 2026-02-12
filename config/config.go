5d5add45239a4fe849af3e1bf5afd5b39016bb3b
Đinh Vạn Tài <vantaidinh04@hotmail.com>
2026-02-12 12:28:36 +0700

    chore(config): update default model to glm-5

--- config/config.go
+++ config/config.go
@@ -66,7 +66,7 @@ func loadConfig() *Config {
 			Think:    getEnv("THINK_TAGS_MODE", "reasoning"),
 		},
 		Model: ModelConfig{
-			Default: getEnv("MODEL", "glm-4.7"),
+			Default: getEnv("MODEL", "glm-5"),
 			Mapping: make(map[string]string),
 		},
 		Headers: make(map[string]string),
